eb150cf974063025e52867932d4e8790bf31338b
David Galvez <davidgalvezgomez01@gmail.com>
2021-11-23 18:01:49 +0100

    Fixed error on .eslintrc.js parsing options

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,10 +8,10 @@ module.exports = {
   extends: ["airbnb-base", "airbnb-typescript/base", "prettier"],
   parser: "@typescript-eslint/parser",
   parserOptions: {
-    ecmaVersion: 12,
+    ecmaVersion: 13,
     sourceType: "module",
     tsconfigRootDir: __dirname,
-    project: ["./tsconfig.json"],
+    project: "./tsconfig.json",
   },
   plugins: ["@typescript-eslint"],
   rules: {
