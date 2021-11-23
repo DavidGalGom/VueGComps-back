3fb5ac495982f91556aca5b0d7ae012cc596e320
David Galvez <davidgalvezgomez01@gmail.com>
2021-11-23 20:14:20 +0100

    Added get route for components

---
+++ server/routes/componentsRoutes.ts
@@ -0,0 +1,9 @@
+import express from "express";
+
+import { getComponents } from "../controller/componentsControllers";
+
+const router = express.Router();
+
+router.get("/", getComponents);
+
+export default router;
