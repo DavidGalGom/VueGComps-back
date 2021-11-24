d6b448ae62cecb15527eb034f47808ed8ff43e0f
David Galvez <davidgalvezgomez01@gmail.com>
2021-11-24 10:46:41 +0100

    Added tests for function getComponents

---
+++ interfaces/response.ts
@@ -0,0 +1,4 @@
+export default interface IResponseTest {
+  status: () => void;
+  json: () => void;
+}
--- server/controller/componentsControllers.ts
+++ server/controller/componentsControllers.ts
@@ -1,8 +1,14 @@
 import Component from "../../database/models/component";
 
-const getComponents = async (req, res) => {
-  const components = await Component.find();
-  res.json(components);
+const getComponents = async (req, res, next) => {
+  try {
+    const components = await Component.find();
+    res.json(components);
+  } catch (error) {
+    error.message = "Can't find the components";
+    error.code = 400;
+    next(error);
+  }
 };
 
 export default getComponents;
