e768dce96ee0aa6c1e78773b6598d86e83116fa5
David Galvez <davidgalvezgomez01@gmail.com>
2021-11-25 11:13:06 +0100

    Added route and controller for updateUser

--- server/routes/usersRoutes.ts
+++ server/routes/usersRoutes.ts
@@ -1,11 +1,17 @@
 import express from "express";
 
-import { getUsers, addUser, loginUser } from "../controller/usersControllers";
+import {
+  getUsers,
+  addUser,
+  loginUser,
+  updateUser,
+} from "../controller/usersControllers";
 
 const router = express.Router();
 
 router.get("/", getUsers);
 router.post("/register", addUser);
 router.post("/login", loginUser);
+router.put("/:idUser", updateUser);
 
 export default router;
